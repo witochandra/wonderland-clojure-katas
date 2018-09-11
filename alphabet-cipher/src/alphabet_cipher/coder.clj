0505b572593e5764afb5d5978d43aa0ec1ca038b
Wito Chandra <wito.c.91@gmail.com>
2018-09-11 23:35:40 +0700

    solve alphabet chiper

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,41 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as str]))
+
+(defn- normalize
+  [c]
+  (- (int c) (int \a)))
+
+(defn- denormalize
+  [i]
+  (char (+ 97 (mod i 26))))
+
+(defn- my-repeat
+  [i u]
+  (->> (repeat (inc (/ (count u) (count i))) i)
+       (apply str)))
 
 (defn encode [keyword message]
-  "encodeme")
+  (->> (cycle keyword)
+       (vector message)
+       (apply map #(+ (normalize %1) (normalize %2)))
+       (map denormalize)
+       (apply str)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (->> (cycle keyword)
+       (vector message)
+       (apply map #(- (normalize %1) (normalize %2)))
+       (map denormalize)
+       (apply str)))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (let [possible-keys (->> [cipher message]
+                           (apply map #(- (normalize %1) (normalize %2)))
+                           (map denormalize)
+                           (reduce #(conj %1 (str (last %1) %2)) []))
+        last-key (last possible-keys)]
+    (->> possible-keys
+         (map #(hash-map :re (my-repeat % last-key) :key %))
+         (filter #(.contains (:re %) last-key))
+         (first)
+         (:key))))
