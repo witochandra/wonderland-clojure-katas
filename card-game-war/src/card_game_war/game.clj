0d48e0b136ab4a4739b566044d14d7339129f4ab
Wito Chandra <wito.c.91@gmail.com>
2018-09-13 21:25:52 +0700

    solve card-game-war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -2,12 +2,33 @@
 
 ;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
+(def suit-values {:spade 0, :club 1, :diamond 2, :heart 3})
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+(def rank-values {2 2, 3 3, 4 4, 5 5, 6 6, 7 7, 8 8, 9 9, 10 10, :jack 11, :queen 12, :king 13, :ace 14})
 (def cards
   (for [suit suits
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn play-round
+  "Returns true when player1's card has higher rank"
+  [player1-card player2-card]
+  (let [player1-num (get rank-values (second player1-card))
+        player1-suit-value ((first player1-card) suit-values)
+        player2-num (get rank-values (second player2-card))
+        player2-suit-value ((first player2-card) suit-values)]
+    (cond (> player1-num player2-num) true
+          (< player1-num player2-num) false
+          (> player1-suit-value player2-suit-value) true
+          (< player1-suit-value player2-suit-value) false
+          :else true)))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-game
+  "Returns true when player2 runs out of cards"
+  [player1-cards player2-cards]
+  (cond (empty? player1-cards) false
+        (empty? player2-cards) true
+        :else (let [player1-card (first player1-cards) player2-card (first player2-cards)]
+                (if (play-round (first player1-cards) (first player2-cards))
+                  (recur (concat (rest player1-cards) `(player1-card player2-card)) (rest player2-cards))
+                  (recur (rest player1-cards) (concat (rest player2-cards) `(player2-card player1-card)))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,22 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (play-round [:spade 10] [:spade 9])))
+  (testing "queens are higher rank than jacks"
+    (is (play-round [:spade :queen] [:spade :jack])))
+  (testing "kings are higher rank than queens"
+    (is (play-round [:spade :king] [:spade :queen])))
+  (testing "aces are higher rank than kings"
+    (is (play-round [:spade :ace] [:spade :king])))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (play-round [:club 9] [:spade 9])))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (play-round [:diamond 9] [:club 9])))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (play-round [:heart 9] [:diamond 9]))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (play-game [[:spade 4] [:spade 5]] [[:spade 2] [:spade 3]]))
+    (is (play-game [[:heart 4] [:heart 5]] [[:spade 4] [:spade 5]]))))
 
